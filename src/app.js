9a468beca3d9eec4d48e71fef9f429cd243d4cd4
kumawat-Ankita <145592877+kumawat-Ankita@users.noreply.github.com>
2024-01-08 10:50:01 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -9,6 +9,11 @@ const tourDetails = JSON.parse(fs.readFileSync(`${__dirname}/data/tours.json`));
 
 app.get('/tours', (req, res) => {
   //write a code here to get all the tours from tours.json
+   res.status(200).json({
+    status: 200,
+    message: 'Success',
+    data: tourDetails,
+  });
 });
 
 app.post('/tours', (req, res) => {
@@ -16,18 +21,84 @@ app.post('/tours', (req, res) => {
   //Write a code here for creating new tour from data/tours.json
   //For creating new id use this logic
   // const newId = tourDetails[tourDetails.length - 1].id + 1;
+  const { name, description, duration, price } = req.body;
+
+  // Create new tour object
+  const newId = tourDetails.length > 0 ? tourDetails[tourDetails.length - 1].id + 1 : 1;
+  const newTour = {
+    id: newId,
+    name,
+    description,
+    duration,
+    price,
+  };
+
+  // Add new tour to tourDetails
+  tourDetails.push(newTour);
+
+  // Write updated data back to tours.json
+  writeTourDataToFile();
+
+  res.status(200).json({
+    status: 200,
+    message: 'Tour added successfully',
+  });
 });
 
 app.put('/tours/:id', (req, res) => {
   const tourId = parseInt(req.params.id);
   const updatedTour = req.body;
+const tourId = parseInt(req.params.id);
+  const updatedTour = req.body;
 
+  // Find the index of the tour to update
+  const tourIndex = tourDetails.findIndex((tour) => tour.id === tourId);
+
+  if (tourIndex !== -1) {
+    // Update tour details
+    tourDetails[tourIndex] = { ...tourDetails[tourIndex], ...updatedTour };
+
+    // Write updated data back to tours.json
+    writeTourDataToFile();
+
+    res.status(200).json({
+      status: 200,
+      message: 'Tour updated successfully',
+    });
+  } else {
+    res.status(404).json({
+      status: 404,
+      message: 'Tour not found',
+    });
+  }
   //write a code here for updating a tour
 });
 
 app.delete('/tours/:id', (req, res) => {
   const tourId = parseInt(req.params.id);
   //Write a code here for deleting a tour from data/tours.json
+  const tourId = parseInt(req.params.id);
+
+  // Find the index of the tour to delete
+  const tourIndex = tourDetails.findIndex((tour) => tour.id === tourId);
+
+  if (tourIndex !== -1) {
+    // Remove tour from tourDetails
+    tourDetails.splice(tourIndex, 1);
+
+    // Write updated data back to tours.json
+    writeTourDataToFile();
+
+    res.status(200).json({
+      status: 200,
+      message: 'Tour deleted successfully',
+    });
+  } else {
+    res.status(404).json({
+      status: 404,
+      message: 'Tour not found',
+    });
+  }
 });
 
 module.exports = app;
